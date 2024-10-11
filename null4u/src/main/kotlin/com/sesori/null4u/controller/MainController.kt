d4827daad9dbe47137654bcdd4ad6a1c4397ce0e
minhee231 <goominhee123@gmail.com>
2024-10-11 17:46:14 +0900

    :sparkles:feat: Hello World 응답하는 메인 컨트롤러 추가

---
+++ null4u/src/main/kotlin/com/sesori/null4u/controller/MainController.kt
@@ -0,0 +1,15 @@
+package com.sesori.null4u.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+//@RequestMapping("/")
+class MainController {
+
+    @GetMapping("/")
+    fun helloWorld(): String {
+        return "Hello World"
+    }
+}
