4ff755b2bda0fa176f64824675f1197ed2f21d37
ororang <102219474+ororang@users.noreply.github.com>
2024-10-11 11:41:57 +0900

    Add files via upload

---
+++ null4u/src/main/kotlin/com/sesori/null4u/Null4uApplication.kt
@@ -0,0 +1,11 @@
+package com.sesori.null4u
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Null4uApplication
+
+fun main(args: Array<String>) {
+	runApplication<Null4uApplication>(*args)
+}
