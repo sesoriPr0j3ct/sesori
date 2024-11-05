dc7a8c10b3dea3a28f7424f022f9ce2799ab30e1
Gooministrator <minhee231>
2024-11-06 01:10:32 +0900

    feat: Spring Security 설정 비활성화
    로그인 너무 귀찮음

--- null4u/src/main/kotlin/com/sesori/null4u/Null4uApplication.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/Null4uApplication.kt
@@ -1,9 +1,10 @@
 package com.sesori.null4u
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class Null4uApplication
 
 fun main(args: Array<String>) {
