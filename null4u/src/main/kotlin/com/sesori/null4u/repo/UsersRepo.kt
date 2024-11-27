cdaa8fb82d2ded9d7df35391286e9e0ff0c693f7
minhee231 <goominhee123@gmail.com>
2024-11-27 11:26:38 +0900

    :hammer:refactor: repository -> repo 이름 변경

--- null4u/src/main/kotlin/com/sesori/null4u/repo/UsersRepo.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/repo/UsersRepo.kt
@@ -1,9 +1,9 @@
-package com.sesori.null4u.repository
+package com.sesori.null4u.repo
 
 import com.sesori.null4u.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UsersRepository : JpaRepository<User, Long> {
+interface UsersRepo : JpaRepository<User, Long> {
 }
--- null4u/src/main/kotlin/com/sesori/null4u/service/UsersService.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/service/UsersService.kt
@@ -1,16 +1,20 @@
 package com.sesori.null4u.service
 
 import com.sesori.null4u.entity.User
-import com.sesori.null4u.repository.UsersRepository
+import com.sesori.null4u.repo.UsersRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class UsersService @Autowired constructor(
-    private val usersRepository: UsersRepository
+    private val usersRepo: UsersRepo
 ) {
 
     fun getAllUsers(): List<User> {
-        return usersRepository.findAll()
+        return usersRepo.findAll()
+    }
+
+    fun createUser(user: User) {
+        usersRepo.save(user)
     }
 }
