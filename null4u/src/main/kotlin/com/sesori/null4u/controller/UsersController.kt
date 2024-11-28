deffc20704b4d3a23229b95ff156bb57e0d7f954
minhee231 <goominhee123@gmail.com>
2024-11-28 17:40:33 +0900

    :hammer:refactor: 유저 컨트롤러 필요없는 프린트 삭제

--- null4u/src/main/kotlin/com/sesori/null4u/controller/UsersController.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/controller/UsersController.kt
@@ -2,24 +2,15 @@ package com.sesori.null4u.controller
 
 import com.sesori.null4u.entity.User
 import com.sesori.null4u.service.UsersService
-import org.apache.coyote.Response
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/users")
 class UsersController(val usersService: UsersService) {
 
     @GetMapping("/list")
     fun getUsers(): List<User> {
-        println("하하하")
         return usersService.getAllUsers()
-    }
-
-    @PostMapping("/create")
-    @ResponseStatus(HttpStatus.OK)
-    fun createUser(@RequestBody user: User) {
-        println("히히히")
-        usersService.createUser(user)
     }
 }
