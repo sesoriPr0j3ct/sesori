97054553875d4331d498c37d9d02684a43098cd9
minhee231 <goominhee123@gmail.com>
2024-11-28 17:41:38 +0900

    :hammer:refactor: 메인 컨트롤러 테스트 매서드 삭제

--- null4u/src/main/kotlin/com/sesori/null4u/controller/MainController.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/controller/MainController.kt
@@ -1,15 +1,9 @@
 package com.sesori.null4u.controller
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
-class TestController() {
-
-    @GetMapping("/test")
-    fun helloWorld() = "Hello World"
-
+class MainController {
 
 }
