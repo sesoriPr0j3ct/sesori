71a56c6cb5f6533882a093e038a5288e6f9e7903
minhee231 <goominhee123@gmail.com>
2024-12-03 15:30:42 +0900

    :hammer:refactor: 허용 origin 하드코딩 수정함

--- null4u/src/main/kotlin/com/sesori/null4u/config/CorsConfig.kt
+++ null4u/src/main/kotlin/com/sesori/null4u/config/CorsConfig.kt
@@ -1,5 +1,6 @@
 package com.sesori.null4u.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -7,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CorsConfig {
+    @Value("\${cors.allowed-origins}")
+    private lateinit var allowedOrigins: String
+
     @Bean
     fun corsConfigurationSource(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**") // 모든 엔드포인트 허용
-                    .allowedOrigins("http://localhost:5173") // 프론트엔드 도메인
+                    .allowedOrigins(*allowedOrigins.split(",").toTypedArray())
                     .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용 HTTP 메서드
                     .allowCredentials(true) // 쿠키 전송 허용
             }
